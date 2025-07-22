6b8cf87c3d684861035348bc195e68b656b8f89d
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-22 17:49:36 +0800

    functions to fetch quote from api, with local quotes being used as fallback

--- quoteGenerator/script.js
+++ quoteGenerator/script.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+// api: https://jacintodesign.github.io/quotes-api/data/quotes.json
+let apiQuotes = [];
+
+const newQuote = () => {
+	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+	console.log(quote);
+};
+
+const getQuotes = async () => {
+	const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+	try {
+		const response = await fetch(apiUrl);
+		apiQuotes = await response.json();
+		newQuote();
+	} catch (error) {
+		// fallback on localQuotes if the api is down or stops working.
+		apiQuotes = localQuotes;
+	}
+};
+
+getQuotes();
