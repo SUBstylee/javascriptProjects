d1d0d7268bf37229a519f534e69387de69a95c3b
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-22 18:37:59 +0800

    api call dom manipulation for text loading spinner logic button logic for tweet button logic for new quote fallback for broken/unresponsive api calls

--- quoteGenerator/script.js
+++ quoteGenerator/script.js
@@ -1,12 +1,35 @@
-// api: https://jacintodesign.github.io/quotes-api/data/quotes.json
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const xBtn = document.getElementById('x');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
 
+const showLoadingSpinner = () => {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+};
+
+const hideLoadingSpinner = () => {
+	loader.hidden = true;
+	quoteContainer.hidden = false;
+};
+
 const newQuote = () => {
+	showLoadingSpinner();
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	console.log(quote);
+	authorText.textContent = !quote.author ? 'Unknown' : quote.author;
+	quote.text.length > 120
+		? quoteText.classList.add('long-quote')
+		: quoteText.classList.remove('long-quote');
+	quoteText.textContent = quote.text;
+	hideLoadingSpinner();
 };
 
 const getQuotes = async () => {
+	showLoadingSpinner();
 	const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 	try {
 		const response = await fetch(apiUrl);
@@ -16,6 +39,18 @@ const getQuotes = async () => {
 		// fallback on localQuotes if the api is down or stops working.
 		apiQuotes = localQuotes;
 	}
+};
+
+const tweetQuote = () => {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(twitterUrl, '_blank');
+};
+
+newQuoteBtn.onclick = () => {
+	newQuote();
+};
+xBtn.onclick = () => {
+	tweetQuote();
 };
 
 getQuotes();
