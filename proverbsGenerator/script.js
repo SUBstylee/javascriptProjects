e72fc17d5cff56a32cd01a6ebba2424b5d8835cb
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-22 19:05:52 +0800

    finalized logic for proverbs

--- proverbsGenerator/script.js
+++ proverbsGenerator/script.js
@@ -1,6 +1,38 @@
-const newProverb = () => {
-	const proverb = proverbs[Math.floor(Math.random() * proverbs.length)];
-	console.log(proverb);
+const proverbContainer = document.getElementById('proverb-container');
+const proverbEnglish = document.getElementById('english');
+const proverbChinese = document.getElementById('chinese');
+const xBtn = document.getElementById('x');
+const newProverbBtn = document.getElementById('new-proverb');
+const loader = document.getElementById('loader');
+
+const showLoadingSpinner = () => {
+	loader.hidden = false;
+	proverbContainer.hidden = true;
 };
 
-newproverb();
+const hideLoadingSpinner = () => {
+	loader.hidden = true;
+	proverbContainer.hidden = false;
+};
+
+const newProverb = () => {
+	showLoadingSpinner();
+	const proverb = proverbs[Math.floor(Math.random() * proverbs.length)];
+	proverbChinese.textContent = proverb.chinese;
+	proverbEnglish.textContent = proverb.english;
+	hideLoadingSpinner();
+};
+
+const tweetQuote = () => {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${proverbEnglish.textContent} - ${proverbChinese.textContent}`;
+	window.open(twitterUrl, '_blank');
+};
+
+newProverbBtn.onclick = () => {
+	newProverb();
+};
+xBtn.onclick = () => {
+	tweetQuote();
+};
+
+newProverb();
