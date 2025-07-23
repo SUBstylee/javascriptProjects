533b604fdb3de707a6ebc7e98e0b86f1aa0d1c59
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-23 11:50:49 +0800

    logic for displaying images using unsplash api

--- 0003infinityScroll/script.js
+++ 0003infinityScroll/script.js
@@ -1,16 +1,43 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 const count = 2;
 const apiKey = 'sbf_CUzUiveoYxKocmpY29R_f0JveEBu3JqnJse-chk';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-const displayPhotos = () => {};
+function setAttributes(element, attributes) {
+	for (const key in attributes) {
+		element.setAttribute(key, attributes[key]);
+	}
+}
 
-const getPhotos = async () => {
+function displayPhotos() {
+	photosArray.forEach((photo) => {
+		const item = document.createElement('a');
+		setAttributes(item, {
+			href: photo.links.html,
+			target: '_blank',
+		});
+
+		const img = document.createElement('img');
+		setAttributes(img, {
+			src: photo.urls.regular,
+			alt: photo.alt_description,
+			title: photo.alt_description,
+		});
+
+		item.appendChild(img);
+		imageContainer.appendChild(item);
+	});
+}
+
+async function getPhotos() {
 	try {
 		const response = await fetch(apiUrl);
-		const data = await response.json();
-		console.log(data);
-	} catch (error) {
-		// Error handling
-	}
-};
-
+		photosArray = await response.json();
+		displayPhotos();
+	} catch (error) {}
+}
 getPhotos();
