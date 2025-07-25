ca9645d1135a586cc5575ea2629dc530446ab300
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-25 12:41:38 +0800

    using navigator to select media steam

--- 0004pictureInPicture/script.js
+++ 0004pictureInPicture/script.js
@@ -1,2 +1,12 @@
 const videoElement = document.getElementById('video');
 const btn = document.getElementById('btn');
+
+const selectMediaStream = async () => {
+	try {
+		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+	} catch (error) {
+		// error catching
+	}
+};
+
+selectMediaStream();
