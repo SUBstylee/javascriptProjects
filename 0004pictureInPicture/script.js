4f7e9b91b480934ef3274f2cf5fff76d1d5fba06
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-25 12:50:38 +0800

    video will begin playing then go into PiP when button clicked

--- 0004pictureInPicture/script.js
+++ 0004pictureInPicture/script.js
@@ -4,9 +4,19 @@ const btn = document.getElementById('btn');
 const selectMediaStream = async () => {
 	try {
 		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+		videoElement.srcObject = mediaStream;
+		videoElement.onloadedmetadata = () => {
+			videoElement.play();
+		};
 	} catch (error) {
 		// error catching
 	}
 };
+
+btn.addEventListener('click', async () => {
+	btn.disabled = true;
+	await videoElement.requestPictureInPicture();
+	btn.disabled = false;
+});
 
 selectMediaStream();
