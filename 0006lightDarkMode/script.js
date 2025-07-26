6576b42240d37ce77156e975ade4408c2b99595c
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-26 17:38:19 +0800

    selected elements that need to be changed when toggling light and dark mode logic for switching themes functions for theme changes

--- 0006lightDarkMode/script.js
+++ 0006lightDarkMode/script.js
@@ -1,7 +1,28 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+const darkMode = () => {
+	nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+	textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+};
+
+const lightMode = () => {
+	nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+	textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+};
 
 const switchTheme = (e) => {
 	console.log(e.target.checked);
+	document.documentElement.setAttribute(
+		'data-theme',
+		e.target.checked ? 'dark' : 'light',
+	);
+	e.target.checked ? darkMode() : lightMode();
 };
 
 toggleSwitch.addEventListener('change', switchTheme);
