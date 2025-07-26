181a966511fb06ae16b66165cb4d824418d6edbf
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-26 17:45:54 +0800

    finished dark and light mode functions

--- 0006lightDarkMode/script.js
+++ 0006lightDarkMode/script.js
@@ -9,15 +9,26 @@ const textBox = document.getElementById('text-box');
 const darkMode = () => {
 	nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
 	textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+	toggleIcon.children[0].textContent = 'Dark Mode';
+	toggleIcon.children[1].classList.remove('fa-sun');
+	toggleIcon.children[1].classList.add('fa-moon');
+	image1.src = 'img/undraw_proud_coder_dark.svg';
+	image2.src = 'img/undraw_conceptual_idea_dark.svg';
+	image3.src = 'img/undraw_feeling_proud_dark.svg';
 };
 
 const lightMode = () => {
-	nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-	textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+	nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+	textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+	toggleIcon.children[0].textContent = 'Light Mode';
+	toggleIcon.children[1].classList.remove('fa-moon');
+	toggleIcon.children[1].classList.add('fa-sun');
+	image1.src = 'img/undraw_proud_coder_light.svg';
+	image2.src = 'img/undraw_conceptual_idea_light.svg';
+	image3.src = 'img/undraw_feeling_proud_light.svg';
 };
 
 const switchTheme = (e) => {
-	console.log(e.target.checked);
 	document.documentElement.setAttribute(
 		'data-theme',
 		e.target.checked ? 'dark' : 'light',
