465f03653adacdb6360414599eb1ea2243551faa
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-28 18:21:08 +0800

    added event listeners for nav hamburger and nav items

--- 0008animatedNavigation/script.js
+++ 0008animatedNavigation/script.js
@@ -5,3 +5,10 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+
+menuBars.addEventListener('click', toggleNav);
+nav1.addEventListener('click', toggleNav);
+nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
+nav4.addEventListener('click', toggleNav);
+nav5.addEventListener('click', toggleNav);
