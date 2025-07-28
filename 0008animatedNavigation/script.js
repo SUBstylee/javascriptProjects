39089c5eb40f146e13756f97fbb3bfe672bf60dd
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-28 19:19:45 +0800

    combined repeated addEventListeners into a forEach

--- 0008animatedNavigation/script.js
+++ 0008animatedNavigation/script.js
@@ -26,8 +26,6 @@ const toggleNav = () => {
 };
 
 menuBars.addEventListener('click', toggleNav);
-nav1.addEventListener('click', toggleNav);
-nav2.addEventListener('click', toggleNav);
-nav3.addEventListener('click', toggleNav);
-nav4.addEventListener('click', toggleNav);
-nav5.addEventListener('click', toggleNav);
+navItems.forEach((nav) => {
+	nav.addEventListener('click', toggleNav);
+});
