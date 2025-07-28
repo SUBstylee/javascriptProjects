13b1ff0263bc92202a48ab650366052354f2a89e
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-28 19:15:47 +0800

    added navAnimation function to replace repeated code

--- 0008animatedNavigation/script.js
+++ 0008animatedNavigation/script.js
@@ -5,24 +5,23 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+const navAnimation = (remove, add) => {
+	navItems.forEach((nav, i) => {
+		nav.classList.replace(`slide-${remove}-${i + 1}`, `slide-${add}-${i + 1}`);
+	});
+};
 
 const toggleNav = () => {
 	menuBars.classList.toggle('change');
 	overlay.classList.toggle('overlay-active');
 	if (overlay.classList.contains('overlay-active')) {
 		overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
-		nav1.classList.replace('slide-out-1', 'slide-in-1');
-		nav2.classList.replace('slide-out-2', 'slide-in-2');
-		nav3.classList.replace('slide-out-3', 'slide-in-3');
-		nav4.classList.replace('slide-out-4', 'slide-in-4');
-		nav5.classList.replace('slide-out-5', 'slide-in-5');
+		navAnimation('out', 'in');
 	} else {
 		overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
-		nav1.classList.replace('slide-in-1', 'slide-out-1');
-		nav2.classList.replace('slide-in-2', 'slide-out-2');
-		nav3.classList.replace('slide-in-3', 'slide-out-3');
-		nav4.classList.replace('slide-in-4', 'slide-out-4');
-		nav5.classList.replace('slide-in-5', 'slide-out-5');
+		navAnimation('in', 'out');
 	}
 };
 
