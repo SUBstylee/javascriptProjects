ce2d7796d828c4e3d1436200323ffc0bf95ae9e0
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-29 16:09:08 +0800

    moved songs array to songs.js progress bar percentage

--- 0009musicPlayer/script.js
+++ 0009musicPlayer/script.js
@@ -2,32 +2,11 @@ const img = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
-
-const songs = [
-	{
-		name: 'song-1',
-		displayName: 'Electric Chill Machine',
-		artist: 'Jacinto Design',
-	},
-	{
-		name: 'song-2',
-		displayName: 'Seven Nation Army (Remix)',
-		artist: 'Jacinto Design',
-	},
-	{
-		name: 'song-3',
-		displayName: 'Goodnight, Disco Queen',
-		artist: 'Jacinto Design',
-	},
-	{
-		name: 'metric-1',
-		displayName: 'Front Row (Remix)',
-		artist: 'Metric/Jacinto Design',
-	},
-];
 
 let isPlaying = false;
 
@@ -70,5 +49,15 @@ const nextSong = () => {
 
 loadSong(songs[currSongIndex]);
 
+const updateProgressBar = (e) => {
+	if (isPlaying) {
+		const { duration, currentTime } = e.srcElement;
+		console.log(duration, currentTime);
+		const progressPercent = (currentTime / duration) * 100;
+		progress.style.width = `${progressPercent}%`;
+	}
+};
+
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
---
+++ 0009musicPlayer/songs.js
@@ -0,0 +1,22 @@
+const songs = [
+	{
+		name: 'song-1',
+		displayName: 'Electric Chill Machine',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'song-2',
+		displayName: 'Seven Nation Army (Remix)',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'song-3',
+		displayName: 'Goodnight, Disco Queen',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'metric-1',
+		displayName: 'Front Row (Remix)',
+		artist: 'Metric/Jacinto Design',
+	},
+];
