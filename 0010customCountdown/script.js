2c2279d096bf443c0f223c5c196d4f0d795864f2
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-07-30 19:03:28 +0800

    added interval so the countdown changes every second

--- 0010customCountdown/script.js
+++ 0010customCountdown/script.js
@@ -10,6 +10,7 @@ const timeElements = document.querySelectorAll('span');
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
+let coundownActive;
 
 const second = 1000;
 const minute = second * 60;
@@ -20,21 +21,23 @@ const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
 
 const updateDOM = () => {
-	const now = new Date().getTime();
-	const distance = countdownValue - now;
+	coundownActive = setInterval(() => {
+		const now = new Date().getTime();
+		const distance = countdownValue - now;
 
-	const days = Math.floor(distance / day);
-	const hours = Math.floor((distance % day) / hour);
-	const minutes = Math.floor((distance % hour) / minute);
-	const seconds = Math.floor((distance % minute) / second);
-	console.log(days, hours, minutes, seconds);
-	inputContainer.hidden = true;
-	countdownEl.hidden = false;
-	countdownElTitle.textContent = `${countdownTitle}`;
-	timeElements[0].textContent = `${days}`;
-	timeElements[1].textContent = `${hours}`;
-	timeElements[2].textContent = `${minutes}`;
-	timeElements[3].textContent = `${seconds}`;
+		const days = Math.floor(distance / day);
+		const hours = Math.floor((distance % day) / hour);
+		const minutes = Math.floor((distance % hour) / minute);
+		const seconds = Math.floor((distance % minute) / second);
+		console.log(days, hours, minutes, seconds);
+		inputContainer.hidden = true;
+		countdownEl.hidden = false;
+		countdownElTitle.textContent = `${countdownTitle}`;
+		timeElements[0].textContent = `${days}`;
+		timeElements[1].textContent = `${hours}`;
+		timeElements[2].textContent = `${minutes}`;
+		timeElements[3].textContent = `${seconds}`;
+	}, second);
 };
 
 const updateCountdown = (e) => {
