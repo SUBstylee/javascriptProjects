a9463cf36441dc8d8fd96836a4c3b60617f912d6
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-01 23:36:53 +0800

    added click event listener to h1 custom button to show modal, by adding class

--- 0011bookKeeper/script.js
+++ 0011bookKeeper/script.js
@@ -1,2 +1,8 @@
 const showModalBtn = document.getElementById('show-modal');
 const modal = document.getElementById('modal');
+
+const showModal = () => {
+	modal.classList.add('show-modal');
+};
+
+showModalBtn.addEventListener('click', showModal);
