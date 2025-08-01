f37e9eed2e202e1407f52544a107a6d88c3aa841
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-01 23:43:25 +0800

    grabbing website url (form)

--- 0011bookKeeper/script.js
+++ 0011bookKeeper/script.js
@@ -3,6 +3,7 @@ const modal = document.getElementById('modal');
 const closeModalBtn = document.getElementById('close-modal');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
 
 const toggleModal = () => {
 	modal.classList.toggle('show-modal');
