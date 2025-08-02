0a878262167042d97ca8cf80dc0bace1efdff8d4
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-02 15:01:23 +0800

    added event listener for save (submit) button storeBookmark function - prepend https:// if not in url.

--- 0011bookKeeper/script.js
+++ 0011bookKeeper/script.js
@@ -1,6 +1,6 @@
-const showModalBtn = document.getElementById('show-modal');
 const modal = document.getElementById('modal');
-const closeModalBtn = document.getElementById('close-modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
@@ -11,8 +11,17 @@ const toggleModal = () => {
 	if (modal.classList.contains('show-modal')) websiteNameEl.focus();
 };
 
-showModalBtn.addEventListener('click', toggleModal);
-closeModalBtn.addEventListener('click', toggleModal);
+const storeBookmark = (e) => {
+	e.preventDefault();
+	const nameVal = websiteNameEl.value;
+	let urlVal = websiteUrlEl.value;
+	if (!urlValue.includes('https://') && !urlValue.includes('http://'))
+		urlValue = `https://${urlValue}`;
+};
+
+modalShow.addEventListener('click', toggleModal);
+modalClose.addEventListener('click', toggleModal);
 window.addEventListener('click', (e) => {
 	e.target === modal ? modal.classList.remove('show-modal') : false;
 });
+bookmarkForm.addEventListener('submit', storeBookmark);
