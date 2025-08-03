fcb136d7f4442c5e2836ee35415db5922224cac1
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-03 23:49:41 +0800

    password validation in JS, and change message accordingly

--- 0013formValidation/script.js
+++ 0013formValidation/script.js
@@ -1,5 +1,43 @@
 const form = document.getElementById('form');
 const password1El = document.getElementById('password1');
 const password2El = document.getElementById('password2');
-const messageContainer = document.querySelector('message-container');
+const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
+
+let isValid = false;
+let passwordsMatch = false;
+
+const validateForm = () => {
+	isValid = form.checkValidity();
+	if (!isValid) {
+		message.textContent = 'Please fill out all fields!';
+		message.style.color = '#f00';
+		messageContainer.style.borderColor = '#f00';
+		return;
+	}
+	if (password1El.value === password2El.value) {
+		passwordsMatch = true;
+		password1El.style.borderColor = '#080';
+		password2El.style.borderColor = '#080';
+	} else {
+		passwordsMatch = false;
+		message.textContent = 'Make sure passwords match!';
+		message.style.color = '#f00';
+		messageContainer.style.borderColor = '#f00';
+		password1El.style.borderColor = '#f00';
+		password2El.style.borderColor = '#f00';
+		return;
+	}
+	if (isValid && passwordsMatch) {
+		message.textContent = 'Successfully Registered!';
+		message.style.color = '#080';
+		messageContainer.style.borderColor = '#080';
+	}
+};
+
+const processFormData = (e) => {
+	e.preventDefault();
+	validateForm();
+};
+
+form.addEventListener('submit', processFormData);
