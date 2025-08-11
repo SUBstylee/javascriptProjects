23054c8bf150d80878f7a0310e4d4fcc0a2c3a95
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-11 16:12:03 +0800

    moved logic into own functions

--- 0023progressSteps/script.js
+++ 0023progressSteps/script.js
@@ -7,18 +7,35 @@ let currentActive = 1;
 const maxSteps = circles.length;
 const splitPercent = 100 / (circles.length - 1);
 
-const moveStep = (dir) => {
-	if (dir !== 'prev' && dir !== 'next') return; // in case something triggers moveStep with an unexpected value
-	dir === 'prev' ? currentActive-- : currentActive++;
-	currentActive = Math.max(1, Math.min(currentActive, maxSteps)); // keeps currentActive in range, even if a user programatically gets past the disabled button, and triggers moveStep
+// keeps currentActive in range, even if a user programatically gets past the disabled button, and triggers moveStep
+const stepClamp = () => {
+	currentActive = Math.max(1, Math.min(currentActive, maxSteps));
+};
+
+// toggle disabled if the beginning or ending of the step range is reached
+const updateButtons = () => {
 	currentActive === 1 ? (prev.disabled = true) : (prev.disabled = false);
 	currentActive === maxSteps ? (next.disabled = true) : (next.disabled = false);
+};
+
+// highlight the current and previously finished steps, adjust width of progress bar
+const updateUI = () => {
 	circles.forEach((circle, i) => {
 		i <= currentActive - 1
 			? circle.classList.add('active')
 			: circle.classList.remove('active');
 	});
 	progress.style.width = `${(currentActive - 1) * splitPercent}%`;
+};
+
+const moveStep = (dir) => {
+	// in case something triggers moveStep with an unexpected value
+	if (dir !== 'prev' && dir !== 'next') return;
+	// increment or decrement the currently active step
+	dir === 'prev' ? currentActive-- : currentActive++;
+	stepClamp();
+	updateButtons();
+	updateUI();
 };
 
 prev.addEventListener('click', () => moveStep('prev'));
