5c36fcff096bff49fce9b0724e379b9f867ab9fe
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-11 16:02:36 +0800

    added condition to exit function early if unexpected value is passed in

--- 0023progressSteps/script.js
+++ 0023progressSteps/script.js
@@ -8,6 +8,7 @@ const maxSteps = circles.length;
 const splitPercent = 100 / (circles.length - 1);
 
 const moveStep = (dir) => {
+	if (dir !== 'prev' && dir !== 'next') return; // in case something triggers moveStep with an unexpected value
 	dir === 'prev' ? currentActive-- : currentActive++;
 };
 
