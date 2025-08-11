85eb4cbe8c11e73ed7a6987692387e315f934639
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-11 15:59:09 +0800

    declared global variables. currentActive will increment and decrement

--- 0023progressSteps/script.js
+++ 0023progressSteps/script.js
@@ -3,5 +3,9 @@ const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 const circles = document.querySelectorAll('.circle');
 
+let currentActive = 1;
+const maxSteps = circles.length;
+const splitPercent = 100 / (circles.length - 1);
+
 prev.addEventListener('click', () => moveStep('prev'));
 next.addEventListener('click', () => moveStep('next'));
