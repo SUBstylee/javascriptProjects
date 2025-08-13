56348bd1bd22e6366bcbd5252d97677e39024690
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-13 14:56:46 +0800

    style calculations

--- 0026blurryLoading/script.js
+++ 0026blurryLoading/script.js
@@ -3,12 +3,17 @@ const bgImg = document.querySelector('.bg');
 
 let load = 0;
 
+const scale = (num, in_min, in_max, out_min, out_max) =>
+	((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+
 const blurring = () => {
 	load++;
 
 	if (load > 99) clearInterval(int);
 
-	console.log(load);
+	loadText.innerText = `${load}%`;
+	loadText.style.opacity = scale(load, 0, 100, 1, 0);
+	bgImg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 };
 
 let int = setInterval(blurring, 30);
