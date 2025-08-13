3cab7ebf8a7cfc332a10054e44358ebfd288a90b
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-13 15:05:07 +0800

    added clamp to load value

--- 0026blurryLoading/script.js
+++ 0026blurryLoading/script.js
@@ -8,14 +8,15 @@ const scale = (num, in_min, in_max, out_min, out_max) =>
 	((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 
 const animate = () => {
-	load++;
+	// clamp for the load value to ensure it never exceeds 100 to prevent unintended styles in css
+	load = Math.min(load + 1, 100);
 
 	if (load > 99) return;
 
 	loadText.innerText = `${load}%`;
 	loadText.style.opacity = scale(load, 0, 100, 1, 0);
 	bgImg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-
+	// instead of setInterval, using requestAnimationFrame recursively for smoother animations
 	requestAnimationFrame(animate);
 };
 
