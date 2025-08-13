79b438ef8894a9c6e7ae45fdaaa28ef0a18768c2
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-13 14:59:27 +0800

    comment to explain how scale works

--- 0026blurryLoading/script.js
+++ 0026blurryLoading/script.js
@@ -3,6 +3,7 @@ const bgImg = document.querySelector('.bg');
 
 let load = 0;
 
+// takes in a number and its min and max values, then balances the transition for a range
 const scale = (num, in_min, in_max, out_min, out_max) =>
 	((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 
