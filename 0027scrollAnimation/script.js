27091ca39e4a03fedee26877a69cfdf14c5915cf
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-14 18:09:27 +0800

    create numBoxes boxes, and append to body

--- 0027scrollAnimation/script.js
+++ 0027scrollAnimation/script.js
@@ -5,3 +5,13 @@ const numBoxes = 50;
 const heading = document.createElement('h1');
 heading.textContent = 'Scroll to see the animation';
 document.body.appendChild(heading);
+
+// create numBoxes boxes, and append to body
+for (let i = 1; i <= numBoxes; i++) {
+	const box = document.createElement('div');
+	box.classList.add('box');
+	const boxText = document.createElement('h2');
+	boxText.textContent = `Content ${i}`;
+	box.appendChild(boxText);
+	document.body.appendChild(box);
+}
