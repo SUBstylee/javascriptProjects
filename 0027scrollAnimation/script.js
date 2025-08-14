7322d3436d59e7ff38c0ca7b60f7df754b66e361
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-14 18:08:53 +0800

    create the h1 heading in js and append to body

--- 0027scrollAnimation/script.js
+++ 0027scrollAnimation/script.js
@@ -1,2 +1,7 @@
 // number of boxes to make
 const numBoxes = 50;
+
+// create the h1 heading and append to body
+const heading = document.createElement('h1');
+heading.textContent = 'Scroll to see the animation';
+document.body.appendChild(heading);
