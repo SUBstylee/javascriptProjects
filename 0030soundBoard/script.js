1aba6af5b14385b79da7331bc5d3fa573f1776a0
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-17 22:33:02 +0800

    stop playing if another button is clicked while audio is playing

--- 0030soundBoard/script.js
+++ 0030soundBoard/script.js
@@ -1,9 +1,19 @@
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
+const stopAudio = () => {
+	sounds.forEach((sound) => {
+		const audio = document.getElementById(sound);
+		song.pause();
+	});
+};
+
 sounds.forEach((sound) => {
 	const btn = document.createElement('button');
 	btn.classList.add('btn');
 	btn.innerText = sound;
-	btn.addEventListener('click', () => document.getElementById(sound).play());
+	btn.addEventListener('click', () => {
+		stopAudio();
+		document.getElementById(sound).play();
+	});
 	document.getElementById('buttons').appendChild(btn);
 });
