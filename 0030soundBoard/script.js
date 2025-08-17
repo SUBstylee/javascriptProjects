1eea372ed2a63204724af7c49499767d40724ac9
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-17 22:29:22 +0800

    click event listener to play audio

--- 0030soundBoard/script.js
+++ 0030soundBoard/script.js
@@ -4,5 +4,6 @@ sounds.forEach((sound) => {
 	const btn = document.createElement('button');
 	btn.classList.add('btn');
 	btn.innerText = sound;
+	btn.addEventListener('click', () => document.getElementById(sound).play());
 	document.getElementById('buttons').appendChild(btn);
 });
