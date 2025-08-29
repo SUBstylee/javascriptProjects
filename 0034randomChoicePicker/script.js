4a952ca8968b1741eb4f5c3a5e74b903c3546a9d
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-29 21:57:06 +0800

    loop through and append a span to tags container for each item in the array

--- 0034randomChoicePicker/script.js
+++ 0034randomChoicePicker/script.js
@@ -8,7 +8,14 @@ const createTags = (input) => {
 		.split(',')
 		.filter((tag) => tag.trim() !== '')
 		.map((tag) => tag.trim());
-	console.log(tags);
+
+	tagsEl.innerHTML = '';
+	tags.forEach((tag) => {
+		const tagEl = document.createElement('span');
+		tagEl.className = 'tag';
+		tagEl.innerText = tag;
+		tagsEl.appendChild(tagEl);
+	});
 };
 
 textarea.addEventListener('keyup', (e) => {
