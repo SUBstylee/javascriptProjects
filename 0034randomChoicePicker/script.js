c27ec0cf54f3c09ef629928479dd8d9877ed07f2
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-29 22:12:10 +0800

    random pick functionality

--- 0034randomChoicePicker/script.js
+++ 0034randomChoicePicker/script.js
@@ -18,7 +18,39 @@ const createTags = (input) => {
 	});
 };
 
-const randomSelect = () => {};
+const pickRandomTag = () => {
+	const tags = document.querySelectorAll('.tag');
+	return tags[Math.floor(Math.random() * tags.length)];
+};
+
+const highlightTag = (tag) => {
+	tag.classList.add('highlight');
+};
+
+const removeHiglightTag = (tag) => {
+	tag.classList.remove('highlight');
+};
+
+const randomSelect = () => {
+	textarea.disabled = true;
+	const times = 30;
+	const interval = setInterval(() => {
+		const randomTag = pickRandomTag();
+		highlightTag(randomTag);
+		setTimeout(() => {
+			removeHiglightTag(randomTag);
+		}, 100);
+	}, 100);
+
+	setTimeout(() => {
+		clearInterval(interval);
+		setTimeout(() => {
+			const randomTag = pickRandomTag();
+			highlightTag(randomTag);
+			textarea.disabled = false;
+		}, 100);
+	}, times * 100);
+};
 
 textarea.addEventListener('keyup', (e) => {
 	createTags(e.target.value);
