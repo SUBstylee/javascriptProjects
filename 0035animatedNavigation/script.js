54895454f1e6afdcd592450bfde91b1ca2a260f0
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-30 23:19:52 +0800

    added commented code for different approaches to achieve the same effect

--- 0035animatedNavigation/script.js
+++ 0035animatedNavigation/script.js
@@ -1,3 +1,16 @@
-document.getElementById('toggle').addEventListener('click', () => {
-	document.getElementById('nav').classList.toggle('active');
-});
+// const toggle = document.getElementById('toggle');
+// const nav = document.getElementById('nav');
+
+// toggle.addEventListener('click', () => nav.classList.toggle('active'));
+
+// document
+// 	.getElementById('toggle')
+// 	.addEventListener('click', () =>
+// 		document.getElementById('nav').classList.toggle('active'),
+// 	);
+
+document
+	.querySelector('#toggle')
+	.addEventListener('click', () =>
+		document.querySelector('#nav').classList.toggle('active'),
+	);
