1dd77657b3d30e77acfdc1643e844ab73614dca1
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-31 23:40:59 +0800

    update big cup

--- 0037drinkWater/script.js
+++ 0037drinkWater/script.js
@@ -1,7 +1,33 @@
-const smallCups = document.querySelectorAll('.cup');
+const bigCup = document.querySelector('.cup-big');
+const smallCups = document.querySelectorAll('.cup-small');
 const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remaining = document.getElementById('remaining');
+
+const updateBigCup = () => {
+	const fullCups = document.querySelectorAll('.cup-small.full').length;
+	console.log(fullCups);
+	const totalCups = smallCups.length;
+	console.log(totalCups);
+	const bigCupHeight = bigCup.offsetHeight;
+
+	if (fullCups === 0) {
+		percentage.style.visibility = 'hidden';
+		percentage.style.height = 0;
+	} else {
+		percentage.style.visibility = 'visible';
+		percentage.style.height = `${(fullCups / totalCups) * bigCupHeight}px`;
+		percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+	}
+
+	if (fullCups === totalCups) {
+		remaining.style.visibility = 'hidden';
+		remaining.style.height = 0;
+	} else {
+		remaining.style.visibility = 'visible';
+		liters.textContent = `${2 - fullCups * 0.25}L`;
+	}
+};
 
 const highlightCups = (idx) => {
 	console.log('clicked');
@@ -20,8 +46,12 @@ const highlightCups = (idx) => {
 		}
 		console.log(cup.classList);
 	});
+
+	updateBigCup();
 };
 
 smallCups.forEach((cup, idx) => {
 	cup.addEventListener('click', () => highlightCups(idx));
 });
+
+updateBigCup();
