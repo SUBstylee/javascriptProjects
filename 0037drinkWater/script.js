45321e218048ce9e307a7402dcdee84ae6b5d5f6
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-31 22:49:49 +0800

    query selectors

---
+++ 0037drinkWater/script.js
@@ -0,0 +1,4 @@
+const smallCups = document.querySelectorAll('.cups');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remaining = document.getElementById('remaining');
