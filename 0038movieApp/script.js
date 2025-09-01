aba2b88e4f593bfcd423b1684198f4eecadab30d
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-01 22:52:16 +0800

    get movies and append to main as children

--- 0038movieApp/script.js
+++ 0038movieApp/script.js
@@ -1,17 +1,52 @@
 const API_KEY = '86a54af6e748c78d4084c2c684eebc58';
 const PAGE_COUNT = 1;
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=${PAGE_COUNT}`;
-const IMG_PATH = 'https://image.tbdb.org/t/p.w1280';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`;
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+const getClassByRate = (rate) => {
+	if (rate >= 8) {
+		return 'good';
+	} else if (rate >= 5) {
+		return 'ok';
+	} else {
+		return 'bad';
+	}
+};
+
+const showMovies = (movies) => {
+	main.innerHTML = '';
+	movies.forEach((movie) => {
+		const { title, poster_path, vote_average, overview } = movie;
+
+		const movieEl = document.createElement('div');
+		movieEl.classList.add('movie');
+		movieEl.innerHTML = `
+		<img src="${IMG_PATH + poster_path}" alt="Poster for ${title}" />
+		<div class="movie-info">
+			<h3>${title}</h3>
+			<span class="${getClassByRate(vote_average)}">${vote_average}</span>
+		</div>
+		<div class="overview">
+			<h3>Overview</h3>
+			<p>
+				${overview}
+			</p>
+		</div>
+        `;
+		main.appendChild(movieEl);
+	});
+};
 
 const getMovies = async (url) => {
 	const res = await fetch(url);
 	const data = await res.json();
 
-	console.log(data.results);
+	showMovies(data.results);
 };
 
 getMovies(API_URL);
