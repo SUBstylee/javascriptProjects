c4fc8f54e01ffcdcf3f23ad723e6d7269809df3c
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-01 22:52:52 +0800

    boost image quality

--- 0038movieApp/script.js
+++ 0038movieApp/script.js
@@ -1,7 +1,7 @@
 const API_KEY = '86a54af6e748c78d4084c2c684eebc58';
 const PAGE_COUNT = 1;
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=${PAGE_COUNT}`;
-const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`;
 
 const form = document.getElementById('form');
