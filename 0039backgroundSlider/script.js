1df37759c15c4d1c898dc27030980e40441064ee
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-02 19:13:17 +0800

    add event listeners to buttons (L/R arrows)

--- 0039backgroundSlider/script.js
+++ 0039backgroundSlider/script.js
@@ -3,3 +3,6 @@ const rightArrow = document.getElementById('right');
 const slides = document.querySelectorAll('.slide');
 
 slides[0].classList.add('active');
+
+leftArrow.addEventListener('click', changeActive('left'));
+rightArrow.addEventListener('click', changeActive('right'));
