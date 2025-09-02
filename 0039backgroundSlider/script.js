76c10a62e96d05d4c5c88cb9503a7c866fb2719a
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-02 19:26:52 +0800

    adjusting slides

--- 0039backgroundSlider/script.js
+++ 0039backgroundSlider/script.js
@@ -3,6 +3,33 @@ const rightArrow = document.getElementById('right');
 const slides = document.querySelectorAll('.slide');
 
 slides[0].classList.add('active');
+let selectedSlide = 0;
+const nSlides = slides.length;
 
-leftArrow.addEventListener('click', changeActive('left'));
-rightArrow.addEventListener('click', changeActive('right'));
+const toggleActive = () => {
+	slides[selectedSlide].classList.toggle('active');
+};
+
+const changeActive = (direction) => {
+	toggleActive();
+	if (direction === 'left') {
+		if (selectedSlide === 0) {
+			selectedSlide = nSlides - 1;
+			toggleActive();
+			return;
+		}
+		selectedSlide--;
+		toggleActive();
+	} else {
+		if (selectedSlide === nSlides - 1) {
+			selectedSlide = 0;
+			toggleActive();
+			return;
+		}
+		selectedSlide++;
+		toggleActive();
+	}
+};
+
+leftArrow.addEventListener('click', () => changeActive('left'));
+rightArrow.addEventListener('click', () => changeActive('right'));
