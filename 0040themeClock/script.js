a13cab7a707e55feadd8b4874430f989d1d2935c
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-03 21:41:11 +0800

    date arrays

--- 0040themeClock/script.js
+++ 0040themeClock/script.js
@@ -6,6 +6,30 @@ const dateEl = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
 const html = document.querySelector('html');
 
+const days = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday',
+];
+const months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
+
 const toggleDarkMode = (e = null) => {
 	const colorMode = window.localStorage.getItem('colorMode');
 	if (!e) {
