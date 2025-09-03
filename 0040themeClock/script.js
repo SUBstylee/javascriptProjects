e9ece8e79db6ed5e5f764196048c2bb08ec1389a
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-03 21:41:44 +0800

    scale function for use with date

--- 0040themeClock/script.js
+++ 0040themeClock/script.js
@@ -30,6 +30,10 @@ const months = [
 	'December',
 ];
 
+const scale = (num, in_min, in_max, out_min, out_max) => {
+	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
 const toggleDarkMode = (e = null) => {
 	const colorMode = window.localStorage.getItem('colorMode');
 	if (!e) {
