d1154cb45cf5b96ec162b5af753554e8dee2df5e
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-05 21:42:53 +0800

    add event listeners

--- 0042dragNDrop/script.js
+++ 0042dragNDrop/script.js
@@ -25,3 +25,13 @@ function dragLeave() {
 function dragDrop() {
 	console.log('drag drop');
 }
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+for (const empty of empties) {
+	empty.addEventListener('dragover', dragOver);
+	empty.addEventListener('dragenter', dragEnter);
+	empty.addEventListener('dragleave', dragLeave);
+	empty.addEventListener('drop', dragDrop);
+}
