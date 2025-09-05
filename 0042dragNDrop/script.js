e1136f074a3d518991881dacd77040742f496543
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-05 21:42:38 +0800

    add function layout, using function declarations so able to use this.

--- 0042dragNDrop/script.js
+++ 0042dragNDrop/script.js
@@ -1,2 +1,27 @@
 const fill = document.querySelector('.fill');
 const empties = document.querySelectorAll('.empty');
+
+function dragStart() {
+	this.className += ' hold';
+	setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+	this.className = 'fill';
+}
+
+function dragOver() {
+	console.log('drag over');
+}
+
+function dragEnter() {
+	console.log('drag enter');
+}
+
+function dragLeave() {
+	console.log('drag leave');
+}
+
+function dragDrop() {
+	console.log('drag drop');
+}
