e96cdcd340e6b7d8cc38f31e5d47220cd5129f8b
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-08 21:49:11 +0800

    add event listener on mousedown, and get x and y positions

--- 0045drawingApp/script.js
+++ 0045drawingApp/script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20;
+let isPressed = false;
 let color = 'black';
 let x;
 let y;
@@ -21,3 +22,9 @@ const drawLine = (x1, y1, x2, y2) => {
 	ctx.lineWidth = size;
 	ctx.stroke();
 };
+
+canvas.addEventListener('mousedown', (e) => {
+	isPressed = true;
+	x = e.offsetX;
+	y = e.offsetY;
+});
