28f3ae078f4edd67523203448f076d56b306df13
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-08 21:47:04 +0800

    setup for drawing shapes

--- 0045drawingApp/script.js
+++ 0045drawingApp/script.js
@@ -1,2 +1,23 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+let size = 20;
+let color = 'black';
+let x;
+let y;
+
+const drawCircle = (x, y) => {
+	ctx.beginPath();
+	ctx.arc(x, y, size, 0, Math.PI * 2, true);
+	ctx.fillStyle = color;
+	ctx.fill();
+};
+
+const drawLine = (x1, y1, x2, y2) => {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.strokeStyle = color;
+	ctx.lineWidth = size;
+	ctx.stroke();
+};
