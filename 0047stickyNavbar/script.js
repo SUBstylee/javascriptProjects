b0fa61fafa3a546246ddb8bbd2b5f837323f195d
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-09-10 21:41:07 +0800

    function to add or remove class based on nav positioning

--- 0047stickyNavbar/script.js
+++ 0047stickyNavbar/script.js
@@ -1,3 +1,11 @@
 const nav = document.querySelector('.nav');
 
+const additionalSpace = 50;
+
+const fixNav = () => {
+	window.scrollY > nav.offsetHeight + additionalSpace
+		? nav.classList.add('active')
+		: nav.classList.remove('active');
+};
+
 window.addEventListener('scroll', fixNav);
